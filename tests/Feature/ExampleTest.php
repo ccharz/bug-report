3e354fe4e9eb0ec2b76a7765273f42f066338334
Christian Charzewski <chris@charzewski.at>
2024-07-24 19:40:17 +0200

    Reproduced route model problem

--- tests/Feature/ExampleTest.php
+++ tests/Feature/ExampleTest.php
@@ -3,17 +3,49 @@
 namespace Tests\Feature;
 
 // use Illuminate\Foundation\Testing\RefreshDatabase;
+
+use App\Models\Order;
+use App\Models\User;
+use Illuminate\Support\Facades\Broadcast;
+use Illuminate\Support\Facades\Config;
+use Illuminate\Support\Facades\Route;
+use Pusher\Pusher;
 use Tests\TestCase;
 
 class ExampleTest extends TestCase
 {
-    /**
-     * A basic test example.
-     */
-    public function test_the_application_returns_a_successful_response(): void
+    private function prepareForPusherTest(): Order
     {
-        $response = $this->get('/');
+        Config::set('broadcasting.default', 'pusher');
+        
+        Broadcast::channel('orders.{order}', function (User $user, Order $order) {
+            return true;
+        });
 
-        $response->assertStatus(200);
+        $user = User::factory()->create();
+
+        $order = Order::create();
+
+        $this->expectExceptionMessageMatches('/Invalid socket ID TEST/');
+        
+        $this
+            ->withoutExceptionHandling()
+            ->actingAs($user)
+            ->post('/broadcasting/auth', ['channel_name' => 'presence-orders.' . $order->id, 'socket_id' => 'TEST']);
+
+
+        return $order;
+    }
+
+    public function test_failing_route_model_binding(): void
+    {   
+        Route::model('order', Order::class);
+        
+        $this->prepareForPusherTest();
+    }
+
+    public function test_working_route_model_binding(): void
+    {         
+        $this->prepareForPusherTest();
     }
 }
